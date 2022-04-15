2f2952491d701cc9ec4eeb707194ad4e7771bcfb
SylumTeam-KoGaMa <103618564+SylumTeam-KoGaMa@users.noreply.github.com>
2022-04-15 17:23:38 -0300

    Create cheat-worker.user.js

---
+++ cheat-worker.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name				KoGaMa Cheat Worker
+// @run-at			    document-start
+// @version			    1.0
+// @description	        KoGaMa Injector
+// @author			    tuba joao pedro
+// @match				https://www.kogama.com/page/webgl-frame/*
+// @match				https://kogama.com.br/page/webgl-frame/*
+// @match				https://friends.kogama.com/page/webgl-frame/*
+// @require             https://raw.githubusercontent.com/SylumTeam-KoGaMa/KoGaMa-Cheat-Worker/main/source.js
+// @grant				none
+// ==/UserScript==
